23d06c115657e683a456bfcb45c1bd91d430661e
SadiqNahmetov <sadigfn@code.edu.az>
2022-09-12 14:33:08 +0400

    Fixed:Products

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,3 +1,127 @@
 let swiper = new Swiper(".mySwiper",{
     loop:true,
 })
+
+
+
+function openTabMenu(evt, TabMenu) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(TabMenu).style.display = "block";
+    evt.currentTarget.className += " active";
+
+  
+  }
+
+
+var swiper1 = new Swiper("#tab-menu .mySwiper1", {
+    slidesPerView: 4,
+    spaceBetween: 30,
+    loop: true,
+
+    autoplay: {
+      delay: 2000,
+      
+    },
+
+  });
+  
+  var swiper2 = new Swiper("#tab-menu .mySwiper2", {
+    slidesPerView: 4,
+    spaceBetween: 30,
+    loop: true,
+    autoplay: {
+      delay: 2000,
+      
+    },
+   
+  });
+  
+  var swiper3 = new Swiper("#tab-menu .mySwiper3", {
+    slidesPerView: 4,
+    spaceBetween: 30,
+    loop: true,
+    autoplay: {
+      delay: 2000,
+      
+    },
+ 
+  });
+  
+  var swiper4 = new Swiper("#tab-menu .mySwiper4", {
+    slidesPerView: 4,
+    spaceBetween: 30,
+    loop: true,
+  
+    autoplay: {
+      delay: 2000,
+      
+    },
+  });
+
+  
+
+
+// function openTabMenu(evt, TabMenu) {
+//     var i, tabcontent, tablinks;
+//     tabcontent = document.getElementsByClassName("tabcontent");
+//     for (i = 0; i < tabcontent.length; i++) {
+//       tabcontent[i].style.display = "none";
+//     }
+//     tablinks = document.getElementsByClassName("tablinks");
+//     for (i = 0; i < tablinks.length; i++) {
+//       tablinks[i].className = tablinks[i].className.replace(" active", "");
+//     }
+//     document.getElementById(TabMenu).style.display = "block";
+//     evt.currentTarget.className += " active";
+//   }
+  
+  
+//   var swiper1 = new Swiper("#tab-menu .mySwiper", {
+//     slidesPerView: 4,
+//     spaceBetween: 30,
+//    loop: true,
+ 
+//   });
+  
+//   var swiper1 = new Swiper("#tab-menu .mySwiper1", {
+//     slidesPerView: 4,
+//     spaceBetween: 30,
+//     loop:true,
+
+  
+//   });
+  
+//   var swiper2 = new Swiper("#tab-menu .mySwiper2", {
+//     slidesPerView: 4,
+//     spaceBetween: 30,
+//     loop:true,
+
+  
+ 
+//   });
+  
+//   var swiper3 = new Swiper("#tab-menu .mySwiper3", {
+//     slidesPerView: 4,
+//     spaceBetween: 30,
+//     loop:true,
+
+  
+   
+//   });
+  
+//   var swiper4 = new Swiper("#tab-menu .mySwiper4", {
+//     slidesPerView: 4,
+//     spaceBetween: 30,
+//     loop:true,
+
+  
+ 
+//   });
