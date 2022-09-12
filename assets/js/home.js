ee09f3258a3d2a244140912112342c9dfbf561f9
SadiqNahmetov <sadigfn@code.edu.az>
2022-09-12 20:40:32 +0400

    Fixed: Top Brands Subscribe

--- assets/js/home.js
+++ assets/js/home.js
@@ -69,59 +69,39 @@ var swiper1 = new Swiper("#tab-menu .mySwiper1", {
   
 
 
-// function openTabMenu(evt, TabMenu) {
-//     var i, tabcontent, tablinks;
-//     tabcontent = document.getElementsByClassName("tabcontent");
-//     for (i = 0; i < tabcontent.length; i++) {
-//       tabcontent[i].style.display = "none";
-//     }
-//     tablinks = document.getElementsByClassName("tablinks");
-//     for (i = 0; i < tablinks.length; i++) {
-//       tablinks[i].className = tablinks[i].className.replace(" active", "");
-//     }
-//     document.getElementById(TabMenu).style.display = "block";
-//     evt.currentTarget.className += " active";
-//   }
-  
-  
-//   var swiper1 = new Swiper("#tab-menu .mySwiper", {
-//     slidesPerView: 4,
-//     spaceBetween: 30,
-//    loop: true,
- 
-//   });
-  
-//   var swiper1 = new Swiper("#tab-menu .mySwiper1", {
-//     slidesPerView: 4,
-//     spaceBetween: 30,
-//     loop:true,
 
-  
-//   });
-  
-//   var swiper2 = new Swiper("#tab-menu .mySwiper2", {
-//     slidesPerView: 4,
-//     spaceBetween: 30,
-//     loop:true,
+$('.brands-slider').slick({
 
+    infinite: true,
+    speed: 300,
+    slidesToShow: 5,
+    slidesToScroll: 5,
+    prevArrow: false,
+      nextArrow: false,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
   
- 
-//   });
-  
-//   var swiper3 = new Swiper("#tab-menu .mySwiper3", {
-//     slidesPerView: 4,
-//     spaceBetween: 30,
-//     loop:true,
-
-  
-   
-//   });
-  
-//   var swiper4 = new Swiper("#tab-menu .mySwiper4", {
-//     slidesPerView: 4,
-//     spaceBetween: 30,
-//     loop:true,
-
-  
- 
-//   });
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+     
+    ]
+  });
