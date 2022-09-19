dba3f38f37b48873b4b6e623e92d82f53595e879
SadiqNahmetov <sadigfn@code.edu.az>
2022-09-19 04:02:57 +0400

    Fixed Front end project

--- assets/js/Home/home.js
+++ assets/js/Home/home.js
@@ -112,7 +112,7 @@ $(function (){
 
   function scrollFunction(){
     if (document.body.scrollTop > 195 || document.documentElement.scrollTop > 195) {
-      scrollSection.style.top = "0";    
+       scrollSection.style.top = "0";    
    } else {
       scrollSection.style.top = "-77px";
       scrollSection.classList.remove("visibl");
